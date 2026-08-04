488124d74173492f343e6b6e9f0b9de79c9a1898
William Poston <willpatpost@gmail.com>
2026-08-03 20:34:51 -0400

    old

--- src/solver/solver.worker.ts
+++
@@ -1,9 +0,0 @@
-import { defaultSolverRegistry } from "./default-registry.ts";
-import {
-  SolverWorkerHost,
-  type SolverWorkerHostTransport,
-} from "./worker-host.ts";
-
-const transport = self as unknown as SolverWorkerHostTransport;
-const host = new SolverWorkerHost(defaultSolverRegistry, transport);
-host.start();
